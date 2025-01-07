87c08e1d538418bcfb1b05de4dacf1f9e12a1a3d
UsmanHussain <mhusmanhussain@gmail.com>
2025-01-07 21:00:24 +0530

    create pages and routes

--- Clinet/src/App.jsx
+++ Clinet/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+
 
 export default function App() {
   return (
-   <h1 className='text-3xl text-red-300'>hiiiii</h1>
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
