6615ae2d7607758debd20a3331f97fbba0707571
Thomiantrooper <128289500+Thomiantrooper@users.noreply.github.com>
2025-01-29 16:29:47 +0530

    Google Auth created and inventory dashboard modified

--- Clinet/src/pages/Signin.jsx
+++ Clinet/src/pages/Signin.jsx
@@ -7,6 +7,7 @@ import {
   signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({ email: '', password: '' }); // ✅ Fix: Set initial state
@@ -99,9 +100,10 @@ export default function SignIn() {
                 'Sign In'
               )}
             </Button>
+            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Don't have an account?</span>
+            <span> Don't have an account?</span>
             <Link to='/signup' className='text-blue-500'>
               Sign Up
             </Link>
