a0ee992f6aa11038970e0816d1178b6a02526479
Thomiantrooper <128289500+Thomiantrooper@users.noreply.github.com>
2025-02-02 20:05:58 +0530

    Save progress before pulling

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -25,7 +24,6 @@ const app = express();
 
 // Middleware to parse incoming JSON
 app.use(express.json());
-app.use(cookieParser());
 
 // API Routes
 app.use("/api/user", userRoutes);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { test } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/test", test);
-router.put('/update/:userId', verifyToken, updateUser);
 
 export default router;
