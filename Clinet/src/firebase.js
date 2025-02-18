a512bd33750642bfa954d83b705f78ee27a8e9f2
it22197214@my.sliit.lk <128289500+Thomiantrooper@users.noreply.github.com>
2025-02-18 02:52:12 -0500

    Inventory crud working and the details fetched successfully in the dashInventory page

--- Clinet/src/firebase.js
+++ Clinet/src/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +18,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+const itemsCollection = collection(db, "inventory"); // Firestore collection reference
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+export { db, itemsCollection, auth };
